4408a4ffee565b8a38067c3f077612059a367759
DevOpsPro <devopsprosiva@gmail.com>
2019-03-28 09:29:19 -0700

    updated extension list

--- install_vscode_extensions.sh
+++ install_vscode_extensions.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 for i in robertohuertasm.vscode-icons ms-python.python redhat.vscode-yaml korekontrol.saltstack \
-  mauve.terraform rust-lang.rust eriklynd.json-tools samuelcolvin.jinjahtml eamodio.gitlens yzhang.markdown-all-in-one
+  mauve.terraform rust-lang.rust eriklynd.json-tools samuelcolvin.jinjahtml \
+  eamodio.gitlens yzhang.markdown-all-in-one ritwickdey.liveserver
 #vscodevim.vim 
 do
   code --install-extension $i

d12ceee63badfbf6ab4fd39329deda78c8aab425
DevOpsPro <devopsprosiva@gmail.com>
2019-04-06 22:38:09 -0700

    removed abusaidm.html-snippets

--- install_vscode_extensions.sh
+++ install_vscode_extensions.sh
@@ -2,8 +2,7 @@
 
 for i in robertohuertasm.vscode-icons ms-python.python redhat.vscode-yaml korekontrol.saltstack \
   mauve.terraform rust-lang.rust eriklynd.json-tools samuelcolvin.jinjahtml \
-  eamodio.gitlens yzhang.markdown-all-in-one ritwickdey.liveserver \
-  abusaidm.html-snippets
+  eamodio.gitlens yzhang.markdown-all-in-one ritwickdey.liveserver 
 #vscodevim.vim 
 do
   code --install-extension $i

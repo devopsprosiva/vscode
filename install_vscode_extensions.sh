14313a69d5b3c2b34aacd6586a98a766d799173f
DevOpsPro <devopsprosiva@gmail.com>
2019-05-09 15:25:54 -0700

    added sysoev.language-stylus

--- install_vscode_extensions.sh
+++ install_vscode_extensions.sh
@@ -2,8 +2,8 @@
 
 for i in robertohuertasm.vscode-icons ms-python.python redhat.vscode-yaml korekontrol.saltstack \
   mauve.terraform rust-lang.rust eriklynd.json-tools samuelcolvin.jinjahtml \
-  eamodio.gitlens yzhang.markdown-all-in-one ritwickdey.liveserver octref.vetur sdras.vue-vscode-extensionpack
-#vscodevim.vim 
+  eamodio.gitlens yzhang.markdown-all-in-one ritwickdey.liveserver octref.vetur sdras.vue-vscode-extensionpack \
+  sysoev.language-stylus
 do
   code --install-extension $i
 done

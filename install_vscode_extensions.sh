429552f6d0c2413792826e07ed5c84c3afcf1515
DevOpsPro <devopsprosiva@gmail.com>
2019-05-24 11:40:35 -0700

    toml

--- install_vscode_extensions.sh
+++ install_vscode_extensions.sh
@@ -3,7 +3,7 @@
 for i in robertohuertasm.vscode-icons ms-python.python redhat.vscode-yaml korekontrol.saltstack \
   mauve.terraform rust-lang.rust eriklynd.json-tools samuelcolvin.jinjahtml \
   eamodio.gitlens yzhang.markdown-all-in-one ritwickdey.liveserver octref.vetur sdras.vue-vscode-extensionpack \
-  sysoev.language-stylus
+  sysoev.language-stylus bungcip.better-toml
 do
   code --install-extension $i
 done

7bcc988e870dcfb9dda8879cd7aa5d9b099eb13b
DevOpsPro <devopsprosiva@gmail.com>
2019-08-18 17:14:00 -0700

    updated to use vscode-icons-team.vscode-icons

--- install_vscode_extensions.sh
+++ install_vscode_extensions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in robertohuertasm.vscode-icons ms-python.python redhat.vscode-yaml korekontrol.saltstack \
+for i in vscode-icons-team.vscode-icons ms-python.python redhat.vscode-yaml korekontrol.saltstack \
   mauve.terraform rust-lang.rust eriklynd.json-tools samuelcolvin.jinjahtml \
   eamodio.gitlens yzhang.markdown-all-in-one ritwickdey.liveserver octref.vetur sdras.vue-vscode-extensionpack \
   sysoev.language-stylus bungcip.better-toml formulahendry.auto-close-tag

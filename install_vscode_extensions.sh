d1efccc507300d25842f8182ce2be7f04663733f
DevOpsPro <devopsprosiva@gmail.com>
2019-08-18 17:11:57 -0700

    added formulahendry.auto-close-tag

--- install_vscode_extensions.sh
+++ install_vscode_extensions.sh
@@ -3,7 +3,7 @@
 for i in robertohuertasm.vscode-icons ms-python.python redhat.vscode-yaml korekontrol.saltstack \
   mauve.terraform rust-lang.rust eriklynd.json-tools samuelcolvin.jinjahtml \
   eamodio.gitlens yzhang.markdown-all-in-one ritwickdey.liveserver octref.vetur sdras.vue-vscode-extensionpack \
-  sysoev.language-stylus bungcip.better-toml
+  sysoev.language-stylus bungcip.better-toml formulahendry.auto-close-tag
 do
   code --install-extension $i
 done

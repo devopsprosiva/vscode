807ceea1f52f01688320e9dc4d1fa065c8d5fc20
Sunita Moka <animatormoka@gmail.com>
2019-12-07 17:34:40 -0800

    added vscode-tailwindcss plugin

--- install_vscode_extensions.sh
+++ install_vscode_extensions.sh
@@ -3,7 +3,8 @@
 for i in vscode-icons-team.vscode-icons ms-python.python redhat.vscode-yaml korekontrol.saltstack \
   mauve.terraform rust-lang.rust eriklynd.json-tools samuelcolvin.jinjahtml \
   eamodio.gitlens yzhang.markdown-all-in-one ritwickdey.liveserver octref.vetur sdras.vue-vscode-extensionpack \
-  sysoev.language-stylus bungcip.better-toml formulahendry.auto-close-tag esbenp.prettier-vscode formulahendry.auto-close-tag
+  sysoev.language-stylus bungcip.better-toml formulahendry.auto-close-tag esbenp.prettier-vscode formulahendry.auto-close-tag \
+  bradlc.vscode-tailwindcss
 do
   code --install-extension $i
 done

947e8202b84ed0a487ad9d8c3a491817a75733ef
Sunita Moka <animatormoka@gmail.com>
2019-12-25 15:52:27 -0800

    added postgresql

--- install_vscode_extensions.sh
+++ install_vscode_extensions.sh
@@ -4,7 +4,7 @@ for i in vscode-icons-team.vscode-icons ms-python.python redhat.vscode-yaml kore
   mauve.terraform rust-lang.rust eriklynd.json-tools samuelcolvin.jinjahtml \
   eamodio.gitlens yzhang.markdown-all-in-one ritwickdey.liveserver octref.vetur sdras.vue-vscode-extensionpack \
   sysoev.language-stylus bungcip.better-toml formulahendry.auto-close-tag esbenp.prettier-vscode formulahendry.auto-close-tag \
-  bradlc.vscode-tailwindcss ms-azuretools.vscode-docker
+  bradlc.vscode-tailwindcss ms-azuretools.vscode-docker ckolkman.vscode-postgres
 do
   code --install-extension $i
 done
